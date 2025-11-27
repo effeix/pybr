2b6dd4c71e5c0630251d6900b253304e9a106af8
Gustavo Efeiche <gustavo.efeiche@gmail.com>
2025-11-27 12:12:28 +0100

    Add CI test jobs

---
+++ noxfile.py
@@ -0,0 +1,10 @@
+import nox
+
+nox.options.default_venv_backend = "uv"
+
+@nox.session(python=["3.10", "3.11", "3.12", "3.13"])
+def tests(session):
+    session.install(".")
+    session.install("pytest")
+
+    session.run("pytest")
